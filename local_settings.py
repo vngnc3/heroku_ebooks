cc431efd698ef511b3880ec9d865e1dfc095a64d
izzy <mista.rol@gmail.com>
2021-06-01 06:19:42 +0700

    debugging

--- local_settings.py
+++ local_settings.py
@@ -23,8 +23,8 @@ USER_ACCESS_FILENAME = '' # The MASTODON user credential file you created at ins
 TWITTER_SOURCE_ACCOUNTS = ["vngnc"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
-STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
-TEST_SOURCE = ".txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
+STATIC_TEST = True  # Set this to True if you want to test Markov generation from a static file instead of the API.
+TEST_SOURCE = "new_corpus.txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
 SCRAPE_URL = False  # Set this to true to scrape a webpage.
 SRC_URL = ['http://www.example.com/one', 'https://www.example.com/two']  # A comma-separated list of URLs to scrape
 WEB_CONTEXT = ['span', 'h2']  # A comma-separated list of the tag or object to search for in each page above.
@@ -33,9 +33,9 @@ WEB_ATTRIBUTES = [{'class': 'example-text'}, {}] # A list of dictionaries contai
 ODDS = 2  # How often do you want this to run? 8 = 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
-DEBUG = False  # Set this to False to start Tweeting live
+DEBUG = True  # Set this to False to start Tweeting live
 TWEET_ACCOUNT = "m3m0rydump"  # The name of the account you're tweeting to.
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
-TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
+TWITTER_ARCHIVE_NAME = "tweet.js" #Name of your twitter archive
 IGNORE_RETWEETS = True #If you want to remove retweets
